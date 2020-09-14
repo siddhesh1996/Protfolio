7ec1c74e4e1f9c6433ff2f469ecb98642981754c
Siddhesh Kasar <ksiddhesh96.sk@gmail.com>
2020-09-14 19:51:38 +0530

    Updates

---
+++ precache-manifest.515cff24a5f950fdbd5cc77ecd1eb004.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ad67bb18dd3a5ed44b9c3a00106f25a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7665027ca3f7c42a6f6",
+    "url": "/static/js/2.19bf571f.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.19bf571f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8554982f12c945cb4dd1",
+    "url": "/static/js/main.658f4d4a.chunk.js"
+  },
+  {
+    "revision": "988e7afb743bf02ef8f6",
+    "url": "/static/js/runtime-main.b3ce0de7.js"
+  }
+]);
